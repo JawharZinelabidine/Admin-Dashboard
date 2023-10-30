58d9feb5d413522a67c70bd289badaa494f8ce52
JawharZinelabidine <jawharzedd@gmail.com>
2023-10-30 19:01:02 +0100

    fixed error in schema and connected database with server

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const usersRouter = require('./routes/users')
+const prisma = require("./model/index");
+
 
 const app = express()
 const port = 3000
@@ -9,6 +12,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+const connect = async () => {
+    try {
+        await prisma.$connect()
+        console.log("connected successfully!")
+    }
+    catch (error) {
+        console.log(error, 'not connected')
+    }
+}
+
+connect()
+
+app.use('/api/users', usersRouter)
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
