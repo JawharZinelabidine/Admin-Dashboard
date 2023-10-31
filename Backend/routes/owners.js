96ed0d7cdeda8b47624410d40840af1558c2e19e
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-10-31 16:13:27 +0100

    implemented signin and signup

--- Backend/routes/owners.js
+++ Backend/routes/owners.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const { getOwners, createOwner, signin } = require("../controller/owners");
 
-const { } = require('../controller/owners');
-
-router.route('/')
-    .get()
+router.route("/").get(getOwners).post(createOwner);
+router.route("/signin").post(signin);
 
 module.exports = router;
