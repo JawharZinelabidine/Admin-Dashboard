dc2813047b34ceff589851deb192e4ac2d0d6b80
JawharZinelabidine <jawharzedd@gmail.com>
2023-10-31 08:58:46 +0100

    routes and controllers established

--- Backend/controller/customers.js
+++ Backend/controller/customers.js
@@ -3,11 +3,11 @@ const prisma = require("../model/index");
 
 
 module.exports = {
-    createCustomer: async (req, res) => {
+    getCustomers: async (req, res) => {
         try {
-            await prisma.user.findMany()
+            const customers = await prisma.user.findMany()
 
-            res.status(201).json('success!');
+            res.status(201).json(customers);
         } catch (error) {
             console.error(error);
             res.status(500).send(error);
