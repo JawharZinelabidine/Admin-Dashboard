608c24e2366db47113d89a1927ef6ef320aff984
JawharZinelabidine <jawharzedd@gmail.com>
2023-11-01 15:26:09 +0100

    added city field in restaurant model

---
+++ Backend/prisma/migrations/20231101092516_city/migration.sql
@@ -0,0 +1,58 @@
+/*
+  Warnings:
+
+  - Added the required column `City` to the `Restaurant` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `description` to the `Restaurant` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "City" AS ENUM ('Tunis', 'Sousse', 'Sfax', 'Kairouan', 'Mahdia', 'Monastir', 'Bizerte', 'Touzeur', 'Djerba', 'Nabel', 'Kelibia', 'Gabes');
+
+-- AlterEnum
+ALTER TYPE "Category" ADD VALUE 'French';
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_customerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Reservation" DROP CONSTRAINT "Reservation_customerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Reservation" DROP CONSTRAINT "Reservation_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Restaurant" DROP CONSTRAINT "Restaurant_ownerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_customerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Review" DROP CONSTRAINT "Review_restaurantId_fkey";
+
+-- AlterTable
+ALTER TABLE "Restaurant" ADD COLUMN     "City" "City" NOT NULL,
+ADD COLUMN     "description" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Restaurant" ADD CONSTRAINT "Restaurant_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reservation" ADD CONSTRAINT "Reservation_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reservation" ADD CONSTRAINT "Reservation_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
