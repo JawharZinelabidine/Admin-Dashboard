60e57971d3c8ae0883335435ab3d58b11afcbe17
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-11-06 16:24:40 +0100

    added nodemailer config

---
+++ Backend/utils/mailing.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+module.exports.sendingMail = async ({ from, to, subject, text }) => {
+  try {
+    let mailOptions = {
+      from,
+      to,
+      subject,
+      text,
+    };
+    const transporter = nodemailer.createTransport({
+      qervice: "gmail",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    return await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log(error);
+  }
+};
