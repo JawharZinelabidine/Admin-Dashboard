c1c9b152f0817ae74931b7e8ce30f8123e354299
Jawhar <138862392+JawharZinelabidine@users.noreply.github.com>
2023-11-09 19:01:39 +0100

    created notification methods for owner

--- Backend/routes/customers.js
+++ Backend/routes/customers.js
@@ -2,18 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 
-const { getCustomers, createCustomers, customerSignin, getOneCustomers, getExpoToken ,verifyEmail} = require('../controller/customers');
+const { getCustomers, createCustomers, customerSignin, getOneCustomers, getExpoToken, verifyEmail, checkNotification, removeNotification } = require('../controller/customers');
 
 router.route('/')
     .get(getCustomers)
     .post(createCustomers);
 
- router.route("/verify/:token").get(verifyEmail);
+router.route("/verify/:token").get(verifyEmail);
 
 router.route('/signin')
     .post(customerSignin);
+router.route('/notification/:id')
+    .get(checkNotification)
+    .put(removeNotification)
 router.route('/:id')
     .get(getOneCustomers)
     .put(getExpoToken)
+
 
 module.exports = router;
