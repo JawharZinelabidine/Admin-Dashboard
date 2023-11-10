40f25c12ac6c1219b67e43182d5b887f293742e2
Adam <abejaoui90@gmail.com>
2023-11-10 19:02:40 +0100

    Admin dashboard styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,41 @@
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
 } from "react-router-dom";
-import './App.css'
+import "./App.css";
 import Login from "./components/Login";
-import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import NewApplication from "./components/NewApplication";
+
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Navigate replace to="/login" />} />
-      <Route path="/login" element={<Home />} />
-    </Routes>
-  </Router>
-  )
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <Dashboard />
+            </>
+          }
+        />
+        <Route
+          path="/NewApplication"
+          element={
+            <>
+              <NewApplication />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
