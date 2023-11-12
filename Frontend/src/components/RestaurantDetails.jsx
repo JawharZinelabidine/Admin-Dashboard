84987deefaef95d39475a9e94fc7da177036a9b7
AhmedSomiaa <ahmed.somiaia@gmail.com>
2023-11-12 14:31:11 +0100

    added silder for images

--- Frontend/src/components/RestaurantDetails.jsx
+++ Frontend/src/components/RestaurantDetails.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function RestaurantDetails() {
     const { restaurantId } = useSelector(state => state.restaurant);
     const [restaurant, setRestaurant] = useState({})
+    console.log(restaurant)
 
     const fetchRestaurant = async () => {
         try {
@@ -22,18 +22,10 @@ function RestaurantDetails() {
         fetchRestaurant();
     }, []);
 
-    const sliderSettings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-    };
-
     return (
         <div className="max-w-[800px] p-[20px] overflow-y-auto max-h-screen relative flex flex-col space-y-[20px] text-gray-700 bg-white shadow-md w-10/12 rounded-xl bg-clip-border">
             {/* {Main Image} */}
-            <h6 className="text-lg font-semibold">Main Image</h6>
+            <h6 className="text-lg font-semibold ">Main Image</h6>
             <div className="flex-1  text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40 max-h-80">
                 <img src={restaurant.main_image} className='w-full h-80 shadow-lg rounded-xl shadow-blue-gray-500/40' />
             </div>
@@ -41,31 +33,32 @@ function RestaurantDetails() {
             {/* Menu Images */}
             <div className="mt-4">
                 <h6 className="text-lg font-semibold mb-2">Menu Images</h6>
-                <Slider {...sliderSettings}>
-                    {restaurant.menu_images &&
-                        restaurant.menu_images.map((menuImage, index) => (
-                            <div key={index}>
-                                <img src={menuImage} className="w-full h-32 object-cover rounded-md" alt={`Menu ${index + 1}`} />
-                            </div>
-                        ))}
-                </Slider>
+                <Carousel>
+                    {restaurant.menu_images && restaurant.menu_images.map((menuImage, index) => (
+                        <img key={index} src={menuImage} className="w-full h-50 object-cover rounded-md" alt={`Menu ${index + 1}`} />
+                    ))}
+                </Carousel>
             </div>
 
             {/* Extra Images */}
-            <div className="mt-4">
-                {restaurant.extra_images.length > 0 && <h6 className="text-lg font-semibold mb-2">Extra Images</h6>}
-                <Slider {...sliderSettings}>
-                    {restaurant.extra_images &&
-                        restaurant.extra_images.map((extraImage, index) => (
+            {restaurant.extra_images && restaurant.extra_images.length > 0 && (
+                <div className="mt-4">
+                    <h6 className="text-lg font-semibold mb-2">Extra Images</h6>
+                    <Carousel>
+                        {restaurant.extra_images.map((extraImage, index) => (
                             <div key={index}>
-                                <img src={extraImage} className="w-full h-32 object-cover rounded-md" alt={`Extra ${index + 1}`} />
+                                <img src={extraImage} className="w-full h-80 object-cover rounded-md" alt={`Extra ${index + 1}`} />
                             </div>
                         ))}
-                </Slider>
-            </div>
+                    </Carousel>
+                </div>
+            )}
 
             <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900"> {restaurant.name}</h5>
-            <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit"> {restaurant.description}</p>
+            <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque vitae exercitationem, eligendi ad voluptatem sunt tempora itaque cumque quos eius molestias sit? Ut, facilis expedita dolorum explicabo porro assumenda. Tempora.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi tempora in nemo illo deleniti iure reiciendis, harum, earum voluptate minus laborum tenetur maiores culpa dignissimos non? Dolorem laudantium impedit voluptatibus.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, iste! Numquam earum iure laboriosam quam? Aliquam expedita eaque ratione accusamus non illum eum pariatur officiis itaque? Corporis quo eveniet consequatur!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab harum soluta velit? Placeat sunt ut esse dolores a mollitia, cumque ipsa facere dicta ab quis quasi porro. Asperiores, incidunt atque.</p>
             <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit"> {restaurant.phone_number}</p>
 
         </div>
