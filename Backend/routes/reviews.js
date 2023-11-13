c4f86cc85dbe5c8b414aef9f02100b8f3458e7fd
Jawhar <138862392+JawharZinelabidine@users.noreply.github.com>
2023-11-13 19:00:44 +0100

    ,

--- Backend/routes/reviews.js
+++ Backend/routes/reviews.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isAuthenticated = require('../middlwares/isAuthenticated')
 const isCustomerAuthorized = require('../middlwares/isCustomerAuthorized')
+const isOwnerAuthorized = require('../middlwares/isOwnerAuthorized')
 
 const { getPendingReviews, createReview, getAllReviews } = require('../controller/reviews');
 
@@ -10,6 +11,8 @@ router.route('/')
 router.route('/:restaurantId')
     .post(isAuthenticated, isCustomerAuthorized, createReview)
     .get(getAllReviews)
+router.route('/owner/:restaurantId')
+    .get(isAuthenticated, isOwnerAuthorized, getAllReviews)
 
 
 module.exports = router;
