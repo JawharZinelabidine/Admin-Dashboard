dcf56d00004319b95a552d539516ce31b03acfc7
Jawhar <138862392+JawharZinelabidine@users.noreply.github.com>
2023-11-13 14:31:48 +0100

    fixed authorization issue

--- Frontend/src/components/NewApplication.jsx
+++ Frontend/src/components/NewApplication.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from "../services/axiosInterceptor";
 import Navbar from './Navbar';
 import PendingRestaurant from './PendingRestaurant';
 import RestaurantDetails from './RestaurantDetails';
