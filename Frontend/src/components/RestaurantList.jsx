18ed1a61d09a144a72799c0841dc8006b4fde525
Oumaima-chiha <chihaouma@gmail.com>
2023-11-14 10:54:50 +0100

    fixing bug

--- Frontend/src/components/RestaurantList.jsx
+++ Frontend/src/components/RestaurantList.jsx
@@ -9,39 +9,32 @@ const RestaurantList = () => {
 
   const getRestaurants = async () => {
     try {
-      const restaurantData = await axios.get(
-        "http://localhost:3000/api/restaurants"
+      const { data } = await axios.get("http://localhost:3000/api/restaurants");
+      setRestaurants(data);
+  
+
+      const restaurantsWithOwners = await Promise.all(
+        Array.isArray(data)
+          ? data.map(async (restaurant) => {
+              const ownerResponse = await axios.get(`http://localhost:3000/api/owners/${restaurant.ownerId}`);
+              const owner = ownerResponse.data;
+              return { ...restaurant, owner };
+            })
+          : []
       );
-      console.log("Restaurant Data:", restaurantData);
-
-      const owners = restaurantData.map(async (restaurant) => {
-        try {
-          const ownerData = await axios.get(
-            `http://localhost:3000/api/owners/${restaurant.ownerId}`
-          );
-          const owner = ownerData.data;
-          return { ...restaurant, owner };
-        } catch (ownerError) {
-          console.error("Error fetching owner:", ownerError);
-          return { ...restaurant, owner: null };
-        }
-      });
-
-      const restaurantsWithOwners = await Promise.all(owners);
-      console.log("Restaurants with Owners:", restaurantsWithOwners);
-
+      
+  
       setRestaurants(restaurantsWithOwners);
     } catch (error) {
-      console.log("Error fetching restaurants:", error);
-
-      if (
-        error.response &&
-        (error.response.status === 403 || error.response.status === 401)
-      ) {
+      console.log(error);
+  
+      if (error.response.status === 403 || error.response.status === 401) {
         localStorage.clear();
+       
       }
     }
   };
+  
   useEffect(() => {
     getRestaurants();
   }, []);
