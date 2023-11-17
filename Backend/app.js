bcb79eb091627af1aca810900f2fe67f36aa0bc6
JawharZinelabidine <jawharzedd@gmail.com>
2023-11-17 02:27:40 +0100

    created socket server

--- Backend/app.js
+++ Backend/app.js
@@ -10,14 +10,11 @@ const messagesRouter = require('./routes/messages')
 const paymentsRouter = require('./routes/payments')
 const prisma = require("./model/index");
 var bodyParser = require('body-parser');
+
+
 const app = express()
 
-const http = require('http').Server(app);
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
-});
+
 
 const port = 3000
 
@@ -42,12 +39,6 @@ connect()
 
 
 
-socketIO.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
-    socket.on('disconnect', () => {
-        console.log('🔥: A user disconnected');
-    });
-});
 
 
 app.use('/api/admin', adminRouter)
@@ -62,3 +53,6 @@ app.use('/api/payments', paymentsRouter)
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
 })
+
+
+
