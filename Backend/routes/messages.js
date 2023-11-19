d7921ead5bb954559136f56799340e61e559f53f
JawharZinelabidine <jawharzedd@gmail.com>
2023-11-19 21:48:07 +0100

    added premium authorization and fixed bug related to fetching upcoming reservations

--- Backend/routes/messages.js
+++ Backend/routes/messages.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isAuthenticated = require('../middlwares/isAuthenticated')
 const isCustomerAuthorized = require('../middlwares/isCustomerAuthorized')
-
+const isPremiumAuthorized = require('../middlwares/isPremiumAuthorized')
 const isOwnerAuthorized = require('../middlwares/isOwnerAuthorized')
 
 const { ownerSend, customerSend, getRestaurantConversations, getCustomerConversations, getRestaurantMessages,
@@ -10,15 +10,15 @@ const { ownerSend, customerSend, getRestaurantConversations, getCustomerConversa
 
 
 router.route('/owner/:customerId')
-    .post(isAuthenticated, isOwnerAuthorized, ownerSend)
+    .post(isAuthenticated, isOwnerAuthorized, isPremiumAuthorized, ownerSend)
 router.route('/customer/:restaurantId')
     .post(isAuthenticated, isCustomerAuthorized, customerSend)
 router.route('/owner/conversations')
-    .get(isAuthenticated, isOwnerAuthorized, getRestaurantConversations)
+    .get(isAuthenticated, isOwnerAuthorized, isPremiumAuthorized, getRestaurantConversations)
 router.route('/customer/conversations')
     .get(isAuthenticated, isCustomerAuthorized, getCustomerConversations)
 router.route('/owner/messages/:customerId')
-    .get(isAuthenticated, isOwnerAuthorized, getRestaurantMessages)
+    .get(isAuthenticated, isOwnerAuthorized, isPremiumAuthorized, getRestaurantMessages)
 router.route('/customer/messages/:restaurantId')
     .get(isAuthenticated, isCustomerAuthorized, getCustomerMessages)
 
